e7baa6b2db13fa5cdec6b0f098215bbf0be672fd
cheeshee <amyliu1.618@gmail.com>
2020-07-16 17:33:50 -0600

    create basic dialogue function

--- Consumer-Game/Assets/Scripts/Tools/Dialogue/DialogueController.cs
+++ Consumer-Game/Assets/Scripts/Tools/Dialogue/DialogueController.cs
@@ -1,34 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using SimpleJSON;
 
 public class DialogueController : MonoBehaviour
 {
-    Collider2D playerDetectionCollider;
+    private Collider2D playerDetectionCollider;
+    private GameObject indicator;
+    private GameObject dialogueCanvas;
     bool inTalkingRange;
+    bool inDialogue;
+
+    [SerializeField]
+    private TextAsset textFile;
+    private JSONObject textJSON;
+
+    // dialgoue progress variables
+    private int stage;              // mark stages or event triggers
+    private float currentConvoID;   // mark conversation to be held at current stage, s.x where s is the stage number, 
+    //currently max 10 conversations each stage change above as needed
+   
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         playerDetectionCollider = GetComponent<Collider2D>();
+        indicator = transform.GetChild(0).gameObject;
+        indicator.SetActive(false);
+        dialogueCanvas = GameObject.FindGameObjectWithTag("DialogueCanvas");
+        dialogueCanvas.SetActive(false);
         inTalkingRange = false;
+        inDialogue = false;   
+
+        // load the dialogue text
+        loadDialogueJSON();
+   
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(inTalkingRange){
+            EnterDialogue();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other){
         if (other.gameObject.layer == (int) Layers.Player){
-            inTalkingRange = true;
             Debug.Log("in talking range");
+            inTalkingRange = true;
+            indicator.SetActive(true);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.layer == (int) Layers.Player){
-            inTalkingRange = false;
             Debug.Log("exiting talking range");
+            inTalkingRange = false;
+            indicator.SetActive(false);
         }
+    }
+
+    private void EnterDialogue(){
+        if (Input.GetButtonDown("Interact")){
+            Debug.Log("Entering Dialogue");
+            // Setting UI components
+            inDialogue = true;            
+            dialogueCanvas.SetActive(true);
+            // Pause player controls TODO
+            
+            // Pass lines to be displayed
+            dialogueCanvas.GetComponent<DialogueDisplayController>().FeedLines(textJSON["stages"][stage][(int)(currentConvoID - stage) * 10].AsArray);
+        }
+        indicator.SetActive(!dialogueCanvas.activeSelf);
+    }
+
+    private void createSampleJSON(){
+         //test // Testing
+        JSONObject yeet = new JSONObject();
+        yeet.Add("currentStage", 0);
+        yeet.Add("currentConvoID", 0.0);
+
+        JSONArray conversations = new JSONArray();
+        JSONArray testlines = new JSONArray();
+        JSONArray Singlelines = new JSONArray();
+        Singlelines.Add("speaker", "anon");
+        Singlelines.Add("line", "i literally wanna die");
+        testlines.Add(Singlelines);
+        Singlelines = new JSONArray();
+        Singlelines.Add("speaker", "anon2");
+        Singlelines.Add("line", "very cool");
+        testlines.Add(Singlelines);
+        conversations.Add(testlines);
+        JSONArray stages = new JSONArray();
+        stages.Add(conversations);
+        stages.Add(conversations);
+        
+        yeet.Add("stages", stages);
+        Debug.Log(yeet.ToString());
+        string path = Application.dataPath + "/Dialogue/test.json";
+        File.WriteAllText(path, yeet.ToString());
+    }
+    private void loadDialogueJSON(){
+        textJSON = (JSONObject) JSON.Parse(textFile.ToString());
+        // will have to change and check these as we go, possibly skip to next stage
+        // TODO
+        stage = textJSON["currentStage"];
+        currentConvoID = textJSON["currentConvoID"];    
     }
 }
---
+++ Consumer-Game/Assets/Scripts/Tools/Dialogue/DialogueDisplayController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+
+public class DialogueDisplayController : MonoBehaviour
+{
+    private GameObject speakerBox;
+    private Text speakerName;
+    private GameObject textBox;
+    private Text dialogueText;
+    private JSONArray conversation;
+
+    private enum Line{SPEAKER_NAME, SENTENCE}
+    private int sentenceIndex;         // index for sentence in conversation
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speakerBox = transform.GetChild(0).gameObject;
+        speakerName = speakerBox.GetComponentInChildren<Text>();
+        textBox = transform.GetChild(1).gameObject;
+        dialogueText = textBox.GetComponentInChildren<Text>();
+        sentenceIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetectNextLine();
+    }
+
+    public int FeedLines(JSONArray lines){
+        Debug.Log("displaying text");
+        Debug.Log(lines);
+        conversation = lines;
+        speakerName.text = conversation[sentenceIndex][(int)Line.SPEAKER_NAME];
+        dialogueText.text = conversation[sentenceIndex][(int)Line.SENTENCE];
+        
+
+        return 0;
+    }
+
+    private void DetectNextLine(){
+        if (Input.anyKeyDown){
+            sentenceIndex++;
+            if (sentenceIndex < conversation.Count) {
+                speakerName.text = conversation[sentenceIndex][(int)Line.SPEAKER_NAME];
+                dialogueText.text = conversation[sentenceIndex][(int)Line.SENTENCE];
+            }
+            else {
+                gameObject.SetActive(false);
+            }
+
+        }
+    }
+}
