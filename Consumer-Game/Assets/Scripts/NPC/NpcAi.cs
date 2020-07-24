cd801817e3a7fa5720b92b53b0a804e5f7510592
cheeshee <amyliu1.618@gmail.com>
2020-07-23 19:16:23 -0600

    create pacingNpc class

--- Consumer-Game/Assets/Scripts/NPC/NpcAi.cs
+++ Consumer-Game/Assets/Scripts/NPC/NpcAi.cs
@@ -7,69 +7,23 @@ public class NpcAi : MonoBehaviour
 {
     // TODO: add variables for health and attack damage, etc.
     
-    // AI path finding variables
-    public Transform target;
 
-    public float speed = 100f;
-    public float nextWaypointDistance = 1f;
+    protected Rigidbody2D rb;
 
-    Path path;
-    int currentWaypoint = 0;
-    bool reachedEndOfPath = false;
-
-    Seeker seeker;
-    Rigidbody2D rb;
-
-    // public Transform npcGraphics;
-    SpriteRenderer npcGraphics;
+    protected SpriteRenderer npcGraphics;
 
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
-        seeker = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
         npcGraphics = GetComponent<SpriteRenderer>();
-
-        InvokeRepeating("UpdatePath", 0f, 0.5f);
     }
 
-    void UpdatePath(){
-        if (seeker.IsDone())
-            seeker.StartPath(rb.position, target.position, OnPathComplete);
-    }
-
-    void OnPathComplete(Path p){
-        if(!p.error){
-            path = p;
-            currentWaypoint = 0;
-        }
-    }
+    
 
     // Update is called once per frame
-    void FixedUpdate()
+    protected virtual void FixedUpdate()
     {
-        if(path == null)
-            return;
-        if(currentWaypoint >= path.vectorPath.Count){
-            reachedEndOfPath = true;
-            Debug.Log("reached end of path");
-            return;
-        } else {
-            reachedEndOfPath = false;
-        }
-
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
-        Vector2 velocity = direction * speed * Time.deltaTime;
-        // Debug.Log(force);
-        // rb.AddForce(force);
-        rb.velocity = velocity;
-
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
-
-        if(distance < nextWaypointDistance){
-            currentWaypoint++;
-        }
-
         if (rb.velocity.x >= 0.01f){
             npcGraphics.flipX = false;
         } else if (rb.velocity.x < -0.01f){
---
+++ Consumer-Game/Assets/Scripts/NPC/PacingNpcAi.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class PacingNpcAi : NpcAi
+{
+    // TODO: add variables for health and attack damage, etc.
+    
+    // AI path finding variables
+    // public Transform target;
+    protected Vector2 targetPosition;
+    // [DraggablePoint]
+    protected Vector2 FirstTargetPosition;
+    // [DraggablePoint]
+    protected Vector2 secondTargetPosition;
+    protected int currentTarget;
+
+    [SerializeField]
+    protected float speed = 100f;
+    [SerializeField]
+    protected float nextWaypointDistance = 0.5f;
+    [SerializeField]
+    protected float pathStartTime = 0f;
+    [SerializeField]
+    protected float strollTime = 10f;  // update path/change destination every _ seconds
+
+    protected Path path;
+    protected int currentWaypoint = 0;
+    protected bool reachedEndOfPath = false;
+
+    protected Seeker seeker;
+    // protected Rigidbody2D rb;
+    // protected SpriteRenderer npcGraphics;
+
+    protected Vector2 direction;
+    protected float velocityX;
+    protected float distToWaypoint; 
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        seeker = GetComponent<Seeker>();
+        targetPosition = FirstTargetPosition;
+        currentTarget = 1;
+        InvokeRepeating("UpdatePath", pathStartTime, strollTime);
+    }
+
+    protected virtual void UpdatePath(){
+        if (seeker.IsDone()){
+            if(currentTarget == 1){
+                currentTarget = 2;
+                targetPosition = secondTargetPosition;
+            } else if (currentTarget == 2){            
+                currentTarget = 1;
+                targetPosition = FirstTargetPosition;
+            }
+            Debug.Log(currentTarget);
+            seeker.StartPath(rb.position, targetPosition, OnPathComplete);
+        }
+    }
+
+    protected virtual void OnPathComplete(Path p){
+        if(!p.error){
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    // Update is called once per frame
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if(path == null)
+            return;
+        if(currentWaypoint >= path.vectorPath.Count){
+            reachedEndOfPath = true;
+            Debug.Log("reached end of path");
+            rb.velocity = new Vector2(0,0);
+            return;
+        } else {
+            reachedEndOfPath = false;
+        }
+
+        direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        velocityX = direction.x * speed * Time.deltaTime;
+        rb.velocity = new Vector2(velocityX, rb.velocity.y);
+
+        distToWaypoint = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distToWaypoint < nextWaypointDistance){
+            currentWaypoint++;
+        }
+
+    }
+}
